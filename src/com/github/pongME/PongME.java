baf7e654d637f0c4b104d9212b3729533b5e0812
max-m <maximilian.mader@gmx.de>
2011-11-11 22:07:35 +0100

    Readded files ... damn Windows -.-

---
+++ src/com/github/pongME/PongME.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates and open the template in the editor.
+ */
+package com.github.janpath.pongME;
+
+import javax.microedition.midlet.*;
+import javax.microedition.lcdui.*;
+
+/**
+ * @author Max
+ */
+public class PongME extends MIDlet {
+
+	public Display display;
+	public PongCanvas canvas = null;
+
+	public void startApp() {
+		if (canvas == null) {
+			canvas = new PongCanvas(this);
+		}
+		Display display = Display.getDisplay(this);
+		display.setCurrent(canvas);
+	}
+
+	public void pauseApp() {
+	}
+
+	public void destroyApp(boolean unconditional) {
+	}
+}
